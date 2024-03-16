bb90d490842d471cf79c8cb54f9d1d2cff641965
bartoszmzywa <bartosz.mzywa@gmail.com>
2024-03-16 19:29:04 +0100

    counting letters

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     words = count_words(text)
-    print(words)
+    letters = count_letters(text)
+    print(letters)
 
 
 def get_book_text(path):
@@ -13,6 +14,21 @@ def get_book_text(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_letters(text):
+    lower_text = text.lower()
+    
+    let_count_dict = {}
+    for i in lower_text:
+        if i not in let_count_dict:
+            number = lower_text.count(i)
+            let_count_dict[i] = number
+        else:
+            continue
+
+
+    return let_count_dict
+
 
 
 
